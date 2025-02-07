574db012272acef3c07fcd37ef4f9e4c5ec25995
amelia-mara <amelia-mara@outlook.com>
2025-02-07 23:03:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     makeWidgetsDraggable();
 });
 
+/* Load project info */
 function loadProjectDashboard() {
     const params = new URLSearchParams(window.location.search);
     const projectName = params.get("project");
@@ -23,41 +24,63 @@ function loadProjectDashboard() {
     loadWidgetPositions();
 }
 
+/* Go back to the main page */
 function goBack() {
     window.location.href = "index.html";
 }
 
+/* Make widgets draggable (for both mouse & touch) */
 function makeWidgetsDraggable() {
     const widgets = document.querySelectorAll(".widget");
 
     widgets.forEach(widget => {
-        widget.onmousedown = function (event) {
-            widget.style.position = "absolute";
-            widget.style.zIndex = 1000;
-
-            function moveAt(pageX, pageY) {
-                widget.style.left = pageX - widget.offsetWidth / 2 + "px";
-                widget.style.top = pageY - widget.offsetHeight / 2 + "px";
-            }
-
-            moveAt(event.pageX, event.pageY);
-
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY);
-            }
-
-            document.addEventListener("mousemove", onMouseMove);
-
-            widget.onmouseup = function () {
-                document.removeEventListener("mousemove", onMouseMove);
-                widget.onmouseup = null;
-                saveWidgetPositions();
-            };
-        };
+        widget.style.position = "absolute"; // Ensures the widgets can be moved freely
+        widget.addEventListener("touchstart", startDrag, false);
+        widget.addEventListener("mousedown", startDrag, false);
     });
+
+    function startDrag(event) {
+        event.preventDefault();
+
+        let widget = event.target.closest(".widget");
+        if (!widget) return;
+
+        let shiftX, shiftY;
+
+        if (event.type === "touchstart") {
+            let touch = event.touches[0];
+            shiftX = touch.clientX - widget.getBoundingClientRect().left;
+            shiftY = touch.clientY - widget.getBoundingClientRect().top;
+
+            document.addEventListener("touchmove", onMove, false);
+            document.addEventListener("touchend", stopMove, false);
+        } else {
+            shiftX = event.clientX - widget.getBoundingClientRect().left;
+            shiftY = event.clientY - widget.getBoundingClientRect().top;
+
+            document.addEventListener("mousemove", onMove, false);
+            document.addEventListener("mouseup", stopMove, false);
+        }
+
+        function onMove(event) {
+            let pageX = event.type === "touchmove" ? event.touches[0].clientX : event.clientX;
+            let pageY = event.type === "touchmove" ? event.touches[0].clientY : event.clientY;
+
+            widget.style.left = pageX - shiftX + "px";
+            widget.style.top = pageY - shiftY + "px";
+        }
+
+        function stopMove() {
+            document.removeEventListener("mousemove", onMove, false);
+            document.removeEventListener("mouseup", stopMove, false);
+            document.removeEventListener("touchmove", onMove, false);
+            document.removeEventListener("touchend", stopMove, false);
+            saveWidgetPositions();
+        }
+    }
 }
 
-/* Save Widget Positions */
+/* Save widget positions */
 function saveWidgetPositions() {
     let widgetPositions = {};
     document.querySelectorAll(".widget").forEach(widget => {
@@ -69,12 +92,11 @@ function saveWidgetPositions() {
     localStorage.setItem("widgetPositions", JSON.stringify(widgetPositions));
 }
 
-/* Load Widget Positions */
+/* Load widget positions on page refresh */
 function loadWidgetPositions() {
     let widgetPositions = JSON.parse(localStorage.getItem("widgetPositions")) || {};
     document.querySelectorAll(".widget").forEach(widget => {
         if (widgetPositions[widget.id]) {
-            widget.style.position = "absolute";
             widget.style.left = widgetPositions[widget.id].left;
             widget.style.top = widgetPositions[widget.id].top;
         }
